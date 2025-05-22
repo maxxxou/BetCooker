e41554829de2d9a6262e54c3d39280afec1ee508
Guest User <Guest@iMac-14.local>
2025-05-22 11:24:23 +0200

    first

---
+++ BetCookerTests/BetCookerTests.swift
@@ -0,0 +1,16 @@
+//
+//  BetCookerTests.swift
+//  BetCookerTests
+//
+//  Created by Guest User on 22/05/2025.
+//
+
+import Testing
+
+struct BetCookerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
