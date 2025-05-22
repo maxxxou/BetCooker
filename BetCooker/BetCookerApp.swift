09f7c50a5f15ea11d200417bd57c7e54a01f26b3
Guest User <Guest@iMac-14.local>
2025-05-22 11:54:55 +0200

    init

---
+++ BetCooker/BetCookerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetCookerApp.swift
+//  BetCooker
+//
+//  Created by Guest User on 22/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct BetCookerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetCookerTests/BetCookerTests.swift
@@ -0,0 +1,17 @@
+//
+//  BetCookerTests.swift
+//  BetCookerTests
+//
+//  Created by Guest User on 22/05/2025.
+//
+
+import Testing
+@testable import BetCooker
+
+struct BetCookerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
