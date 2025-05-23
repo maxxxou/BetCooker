89a4b35c813a1d7b7df9688f7a53188cda65285a
Guest User <Guest@iMac-14.local>
2025-05-23 10:11:20 +0200

    pussh inter

--- BetCooker/Views/FavoritesView.swift
+++ BetCooker/Views/FavoritesView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct FavoritesView: View {
     var body: some View {
         ZStack {
-            Color(hex: "#1f1f1f").edgesIgnoringSafeArea(.all)
+            Color(hex: "#000000").edgesIgnoringSafeArea(.all)
             Text("Vos matchs favoris s'afficheront ici.")
                 .foregroundColor(.white)
                 .padding()
